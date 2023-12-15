163159a3e9df8f0d54b86f9879e7858cd10bed08
sryzhkov <sryzhkov@neoflex.ru>
2023-12-15 11:07:10 +0300

    add CustomPrincipal

---
+++ src/main/java/ru/ryzhkov/webfluxsecurity/security/CustomPrincipal.java
@@ -0,0 +1,20 @@
+package ru.ryzhkov.webfluxsecurity.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.security.Principal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomPrincipal implements Principal {
+    private Long id;
+    private String name;
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
